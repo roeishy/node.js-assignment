e15e4dd7174cda5150f0ba1931ee85d5fb314e51
roeishy <roeishy3@gmail.com>
2022-08-28 19:37:13 +0300

    create note api

--- api/note/note.DAO.js
+++ api/note/note.DAO.js
@@ -1,1 +1,18 @@
 const db = require('../../db/db');
+
+module.exports = {
+    createNote,
+};
+
+async function createNote(title, content, author) {
+    const id = await db('note')
+        .insert({
+            title,
+            content,
+            author,
+            is_deleted: false
+        })
+        .returning('id');
+
+    return id;
+}
--- api/note/note.routes.js
+++ api/note/note.routes.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const { addNote } = require('./note.controller')
+
+
+router.post('/', addNote)
 
 
 module.exports = router
