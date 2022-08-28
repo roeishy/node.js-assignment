6e517cdb0c9ddca184716efd259e82d8aa6a2b03
roeishy <roeishy3@gmail.com>
2022-08-28 20:11:25 +0300

    merge issues

--- api/note/note.DAO.js
+++ api/note/note.DAO.js
@@ -8,11 +8,12 @@ module.exports = {
 
 async function deleteNote(id) {
     await db('note').where('id', id).update({ is_deleted: true });
+}
 
 async function getAllNotes() {
     const notes = await db('note').select('id', 'title', 'content', 'author', 'date').where('is_deleted', false).from('note');
     return notes;
-    }
+}
 
 async function createNote(title, content, author) {
     const id = await db('note')
